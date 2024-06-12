1506690be6668209c3f6873b3573057a39a5b85a
Cash Prokop-Weaver <cashweaver@google.com>
2024-06-12 15:47:33 -0700

    :sparkles:: Add manual_sync.sh

---
+++ scripts/sync/manual_sync.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# Syncronize notes across machines
+#
+# This is an alternative to Syncthing after it was banned on corporate devices.
+
+unison \
+  -auto \
+  -batch \
+  -ignore "Name {org-roam.db,org-roam.db-journal}" \
+  -ignore "Regex .unison.*" \
+  /home/cashweaver/proj/notes \
+  "ssh://cashweaver.c.googlers.com//usr/local/google/home/cashweaver/proj/notes"
