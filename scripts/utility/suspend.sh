abcfb84f995650ead12bf7d167a18bf110073377
Cash Prokop-Weaver <cash@cashpw.com>
2024-12-11 08:29:44 -0800

    ♻ s/source/bash/

--- scripts/utility/suspend.sh
+++ scripts/utility/suspend.sh
@@ -3,7 +3,7 @@
 source ~/.scripts/identify_device/identify_device.sh
 
 if is_work_laptop; then
-  source ~/.scripts/utility/lock.sh && systemctl suspend
+  bash ~/.scripts/utility/lock.sh && systemctl suspend
 else
-  source ~/.scripts/utility/lock.sh
+  bash ~/.scripts/utility/lock.sh
 fi
