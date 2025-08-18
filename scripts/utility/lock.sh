d3e33d4d5c34607c4c94be1a96cd8be5eb765476
Cash Prokop-Weaver <cashbweaver@gmail.com>
2025-08-18 05:22:27 -0700

    feat: Support sway/wayland locking

--- scripts/utility/lock.sh
+++ scripts/utility/lock.sh
@@ -1,3 +1,8 @@
 #!/usr/bin/env sh
 
-i3lock --ignore-empty-password --color=1d1f21
+bg_color=1d1f21
+if [[ "$XDG_CURRENT_DESKTOP" == "sway" ]]; then
+  swaylock --ignore-empty-password --color=$bg_color
+elif [[ "$XDG_CURRENT_DESKTOP" == "sway" ]]; then
+  i3lock --ignore-empty-password --color=$bg_color
+fi
