1aded0dad6dff0c57e0498ce26a1634df25fdfcc
Cash Prokop-Weaver <cashweaver@google.com>
2026-03-02 07:36:24 -0800

    fix: Fix lock on work laptop

--- scripts/utility/lock.sh
+++ scripts/utility/lock.sh
@@ -3,6 +3,6 @@
 bg_color=1d1f21
 if [[ "$XDG_CURRENT_DESKTOP" == "sway" ]]; then
   swaylock --ignore-empty-password --color=$bg_color
-elif [[ "$XDG_CURRENT_DESKTOP" == "sway" ]]; then
+else
   i3lock --ignore-empty-password --color=$bg_color
 fi
