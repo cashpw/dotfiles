358d4e187dd221eb2a90b59b046bc2be67fdd89b
Cash Weaver <cashbweaver@gmail.com>
2026-04-27 22:04:53 -0700

    feat: work zsh

--- config/zsh/packages.zsh
+++ config/zsh/packages.zsh
@@ -8,7 +8,7 @@ antigen bundle zsh-users/zsh-completions
 
 antigen bundle zsh-users/zsh-autosuggestions
 
-antigen bundle zsh-users/zsh-syntax-highlighting
+#antigen bundle zsh-users/zsh-syntax-highlighting
 
 antigen bundle zsh-users/zsh-history-substring-search
 bindkey "$terminfo[kcuu1]" history-substring-search-up
@@ -17,11 +17,17 @@ bindkey "$terminfo[kcud1]" history-substring-search-down
 antigen bundle MichaelAquilina/zsh-you-should-use
 
 if [[ $IS_WORK -eq 1 ]]; then
-    # Add work-only bundles here
-    # antigen bundle some/work-plugin
+  # Add work-only bundles here
+  # antigen bundle some/work-plugin
 fi
 
 export NVM_DIR="$HOME/.config/nvm"
 antigen bundle lukechilds/zsh-nvm
+
+#antigen bundle marlonrichert/zsh-edit@main
+
+autoload -Uz edit-command-line
+zle -N edit-command-line
+bindkey '^g' edit-command-line
 
 antigen apply
--- config/zsh/work.zsh
+++ config/zsh/work.zsh
@@ -1,3 +1,5 @@
 #!/usr/bin/env zsh
 
 # Work configuration
+alias jetski-cli='/google/bin/releases/jetski-devs/tools/cli'
+
